67f72e4c501bd00590e68a40f576597936a121d3
liulijun <neteasellj@qq.com>
2023-01-09 21:38:36 +0800

    feat: add command help message

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
+# c2p(Copy to Pasteboard)
+c2p的功能包括：
+- 将文件内容拷贝到粘贴板
+- 预览复制的内容
+- 输出粘贴板中的内容
+
+<br>
+
 # 参考资料
 [Get started with CMake Tools on Linux](https://code.visualstudio.com/docs/cpp/cmake-linux#:~:text=Get%20started%20with%20CMake%20Tools%20on%20Linux%20CMake,tool%20files%20specific%20to%20your%20compiler%20and%20platform.)
+[Boost Source Code](https://github.com/boostorg/boost)
--- main.cpp
+++ main.cpp
@@ -1,14 +1,89 @@
+#include <boost/program_options.hpp>
+
+namespace po = boost::program_options;
+
 #include <iostream>
 #include "ext/clip/clip.h"
+#include <iterator>
+using namespace std;
 
-int main(int, char **)
+// A helper function to simplify the main part.
+template<class T>
+ostream& operator<<(ostream& os, const vector<T>& v)
 {
-    std::cout << "Hello, world!\n";
-    clip::format my_format =
-        clip::register_format("com.appname.FormatName");
+    copy(v.begin(), v.end(), ostream_iterator<T>(os, " "));
+    return os;
+}
 
-    int value = 32;
-    clip::set_text("Hello World112\n haha");
+int main(int ac, char ** av)
+{
+    try {
+        int opt;
+        int portnum;
+        po::options_description optionsDesc("Allowed options");
+        optionsDesc.add_options()
+            ("help,h", "show help message")
+            ("preview,p", "preview copy to paster content")
+            ("show,s", "show paster content")
+            ("file,f", po::value<string>(), "input file")
+            // ("optimization", po::value<int>(&opt)->default_value(10),
+            //       "optimization level")
+            // ("verbose,v", po::value<int>()->implicit_value(1),
+            //       "enable verbosity (optionally specify level)")
+            // ("listen,l", po::value<int>(&portnum)->implicit_value(1001)
+            //       ->default_value(0,"no"),
+            //       "listen on a port.")
+            // ("include-path,I", po::value< vector<string> >(),
+            //       "include path")
+            // ("input-file", po::value< vector<string> >(), "input file")
+        ;
+
+        po::positional_options_description positionalOptionsDesc;
+        positionalOptionsDesc.add("file", 1);
+
+        po::variables_map vm;
+        po::store(po::command_line_parser(ac, av).options(optionsDesc).positional(positionalOptionsDesc).run(), vm);
+        po::notify(vm);
+
+        if (vm.count("help")) {
+            cout << "Usage: c2p [ps] [-f] file\n";
+            cout << optionsDesc;
+            return 0;
+        }
+
+        if (vm.count("include-path"))
+        {
+            cout << "Include paths are: "
+                 << vm["include-path"].as< vector<string> >() << "\n";
+        }
+
+        if (vm.count("input-file"))
+        {
+            cout << "Input files are: "
+                 << vm["input-file"].as< vector<string> >() << "\n";
+        }
+
+        if (vm.count("verbose")) {
+            cout << "Verbosity enabled.  Level is " << vm["verbose"].as<int>()
+                 << "\n";
+        }
+
+        cout << "Optimization level is " << opt << "\n";
+
+        cout << "Listen port is " << portnum << "\n";
+    }
+    catch(std::exception& e)
+    {
+        cout << e.what() << "\n";
+        return 1;
+    }
+    return 0;
+    // std::cout << "Hello, world!\n";
+    // clip::format my_format =
+    //     clip::register_format("com.appname.FormatName");
+
+    // int value = 32;
+    // clip::set_text("Hello World112\n haha");
 
     // clip::lock l;
     // l.clear();
